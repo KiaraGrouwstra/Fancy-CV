ba38c86c1ec469bce15202d3e5b8225d14f2ecc8
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-11-22 11:37:46 +0100

    fix -b switch

--- sync.sh
+++ sync.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 while getopts ":b:" opt; do
-  case $opt in
-    b)
-    build = true
-    echo "found -b switch, build: $build"
-    ;;
-    # \?) echo "Invalid option: -$OPTARG" >&2;;
+  case $OPTARG in
+    b) build=true;;
+    \?) echo "Invalid option: -$OPTARG ($opt)" >&2;;
   esac
 done
 branch=`git rev-parse --abbrev-ref HEAD`
 branches=`ls .git/refs/heads`;
-for BRANCH in $branches;
-    do git checkout $BRANCH;
+for BRANCH in $branches; do
+    git checkout $BRANCH;
     git merge master $BRANCH;
 done;
-if [ "$build" = true ] ; then
-    echo "build is true: $build"
-    for BRANCH in $branches;
-        do git checkout $BRANCH;
+if [ "$build" = true ]; then
+    for BRANCH in $branches; do
+        git checkout $BRANCH;
         bash run.sh;
     done;
 fi
