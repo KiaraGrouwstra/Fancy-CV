394f9dc6c6bc938ff32acdac1becf92d6da7f3f8
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-11-22 15:19:26 +0100

    sync: flatten, check errors, skip master

--- sync.sh
+++ sync.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
+
+# get -p switch
 while getopts ":b:" opt; do
   case $OPTARG in
     b) build=true;;
     \?) echo "Invalid option: -$OPTARG ($opt)" >&2;;
   esac
 done
+
+# test branch switch
+git checkout .
+if [ $? -ne 0 ] then
+    echo "couldn't switch branch!"
+    exit 1
+fi
+
 branches=`ls .git/refs/heads`;
+
+# merge changes
 for BRANCH in $branches; do
-    git checkout $BRANCH;
-    git merge --squash master $BRANCH -m "$BRANCH";
+    if [ $BRANCH -ne "master" ] then
+        git checkout $BRANCH;
+        git merge --squash master $BRANCH -m "merge";
+        git reset --mixed origin/master
+        git commit -am "$BRANCH";
+        if [ $? -ne 0 ] then
+            echo "couldn't merge!"
+            exit 1
+        fi
+    fi
 done;
+
+# if -p rebuild all
 if [ "$build" = true ]; then
     for BRANCH in $branches; do
         git checkout $BRANCH;
         bash run.sh;
     done;
 fi
+
+# return
 git checkout master;
