40118603010474d5432bbe6815e10e0af8f74ab4
Tycho Grouwstra <tychogrouwstra@gmail.com>
2017-11-26 21:59:04 +0100

    merge

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@
 branch=`git rev-parse --abbrev-ref HEAD`
 outdir="build/$branch"
 langs=(en nl)
-for lang in "${langs[@]}"
-do
-  mkdir $outdir
+for lang in "${langs[@]}"; do
+  mkdir -p $outdir
   xelatex -jobname=cv-tycho-$lang -output-directory=$outdir -interaction=nonstopmode "\newif\ifen\newif\ifnl\\${lang}true \input{cv}"
 done
