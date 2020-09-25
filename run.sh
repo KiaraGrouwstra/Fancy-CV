7c274a0a7a8b77afe85d025b710a9932a63a3150
3llobo <flocwolf@gmail.com>
2020-09-25 14:53:30 +0200

    kiara

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ outdir="build/$branch"
 langs=(en nl)
 for lang in "${langs[@]}"; do
   mkdir -p $outdir
-  xelatex -jobname=cv-tycho-$lang -output-directory=$outdir -interaction=nonstopmode "\newif\ifen\newif\ifnl\\${lang}true \input{cv}"
+  xelatex -jobname=cv-kiara-$lang -output-directory=$outdir -interaction=nonstopmode "\newif\ifen\newif\ifnl\\${lang}true \input{cv}"
 done
